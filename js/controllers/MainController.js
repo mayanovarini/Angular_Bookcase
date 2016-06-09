678fe2932103d3486798a50dda0e2af90b61f9cc
mayanovarini@gmail.com <mayanovarini@gmail.com>
2016-06-08 19:47:21 -0700

    Angular app showcasing books

---
+++ js/controllers/MainController.js
@@ -0,0 +1,28 @@
+app.controller('MainController', ['$scope', function($scope) {
+  $scope.title = 'Top Books in Library';
+  $scope.promo = 'Check What\'s on Sale!';
+  $scope.products = [
+    {
+      name: 'The Price of Inequality',
+      price: 1,
+      pubdate: new Date('2010', '01', '01'),
+      cover: 'img/globalization_stiglitz.png'
+      likes: 0,
+      dislikes: 0,
+    },
+    {
+      name: 'Blowback',
+      price: 1,
+      pubdate: new Date('2008', '04', '01'),
+      cover: 'img/blowback.jpg'
+      likes: 0,
+      dislikes: 0,
+    },
+  ];
+  $scope.minusOne = function(index) {
+    $scope.products[index].dislikes += 1;
+  };
+ 	$scope.plusOne = function(index) {
+   $scope.products[index].likes += 1;
+ 	};
+}]);
